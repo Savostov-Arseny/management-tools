985fa689d4a13b13ebd51d5f60a595a5b98f3434
Savostov-Arseny <savostov.arseny@gmail.com>
2020-01-24 13:21:44 +0400

    pip install without confirmation

--- Ansible/host_configure.sh
+++ Ansible/host_configure.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 sudo python get-pip.py
-sudo pip install ansible botocore boto3
+sudo pip install -y ansible botocore boto3
 sudo cp ./conf/ansible.cfg /etc/ansible/ansible.cfg
 sudo cp ./conf/aws_ec2.yml /etc/ansible/ec2.yml
 sudo cp ./Playbooks /etc/ansible/playbooks
