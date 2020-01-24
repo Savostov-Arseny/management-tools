109a98570d41785fdb7119ebcefe2e7293423e5e
Savostov-Arseny <savostov.arseny@gmail.com>
2020-01-24 14:02:42 +0400

    fix typo's

--- Ansible/host_configure.sh
+++ Ansible/host_configure.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 sudo curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 sudo python get-pip.py
-sudo pip install -y ansible botocore boto3
+sudo pip install ansible botocore boto3
+sudo mkdir /etc/ansible
 sudo cp ./conf/ansible.cfg /etc/ansible/ansible.cfg
-sudo cp ./conf/aws_ec2.yml /etc/ansible/ec2.yml
-sudo cp ./Playbooks /etc/ansible/playbooks
+sudo cp ./conf/aws_ec2.yml /etc/ansible/aws_ec2.yml
+sudo cp ./Playbooks -r /etc/ansible/playbooks
